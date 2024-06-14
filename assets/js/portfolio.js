99a4bcd10f6b12d4a0c1aab8bc2d3dc941b40088
Kishor1703 <pskishor196@gmail.com>
2024-06-14 14:33:28 +0530

    certificate

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -22,3 +22,38 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+let slideIndex = 0;
+showSlides();
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n - 1);
+}
+
+function showSlides() {
+    let i;
+    let slides = document.querySelectorAll(".slide");
+    let dots = document.querySelectorAll(".dot");
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } 
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
